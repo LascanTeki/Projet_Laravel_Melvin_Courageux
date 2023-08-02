7162651cbc4ffa6ad983e27d7bfbdeed49c73659
LascanTeki <91802117+LascanTeki@users.noreply.github.com>
2023-08-03 01:18:54 +0200

    feat: :white_check_mark: Changed from Laravel 9 to Laravel 6
    The version of Laravel was changed from 9 to 6, and actions have been made so the site still works in that state

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,12 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
+
+    
     /**
      * Register any application services.
      *
@@ -15,7 +18,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
-
     /**
      * Bootstrap any application services.
      *
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }
